10c2e2d6a4e83c75a049f38ada230e7406d43535
Mateus Leitão <mateuselpidio0@gmail.com>
2022-06-30 17:38:05 -0300

    main principes study init

--- exercicios_dart/app.dart
+++ exercicios_dart/app.dart
@@ -1,17 +1,3 @@
-printProduct(int unit, {String? nome, double? notas}) {
-  for (var i = 0; i < unit; i++) {
-    print('Aluno ${nome} passou com uma nota de ${notas}');
-  }
-}
-
-class Alunos {
-  String? nome;
-  double? notas;
-
-  Alunos({this.nome, this.notas});
-}
-
 main() {
-  var aluno1 = Alunos(nome: 'Joaozinho', notas: 9.8);
-  printProduct(3, nome: aluno1.nome, notas: aluno1.notas);
+  print('Ola');
 }
